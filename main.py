acae3654a1ba3d2414db2a2f6e14450ccc0e3df4
Africa-t0t0 <geoorgex@gmail.com>
2022-04-05 13:44:52 -0400

    TODO: fix aux_y

--- main.py
+++ main.py
@@ -15,12 +15,32 @@ def line_of_sight(x, y, n, board):
     # check diagonal superior derecha
     # for aux_x in reversed(range(0, aux_x+1)):
     #     if aux_y+1<=n:
-    #         print(aux_x, aux_y)
     #         aux_y+=1
     #     if board[aux_x, aux_y-1] != '1':
     #         board[aux_x ,aux_y-1] = '1'
     #     if aux_y == n:
     #         break
+    # check fila
+    # for aux_y in range(0, n):
+    #     if board[aux_x, aux_y] != '1':
+    #         board[aux_x, aux_y] = '1'
+    # check columna
+    # for aux_x in range(0, n):
+    #     if board[aux_x, aux_y] != '1':
+    #         board[aux_x, aux_y] = '1'
+    # check diagonal inferior izquierda
+    for aux_x in range(aux_x, n):           # TODO: Fix aux_y porque tiene +1
+        if aux_y>=0:
+            print('uwu' , aux_x, aux_y)
+            aux_y-=1
+        if board[aux_x, aux_y] != '1':
+            board[aux_x, aux_y] = '1'
+        if aux_y == 0:
+            break
+
+
+
+
 
     #check fila
     #for i in range(n)
@@ -33,14 +53,14 @@ def line_of_sight(x, y, n, board):
     #check diagonal
     #for i in range(n):
     #   if board[x,i] == 1:
-    #       
+    #
 
 def n_queens():
     n=4
     queens = 0
     new_tree = Tree()
     board = np.zeros((n, n), dtype=int)
-    line_of_sight(1, 2, n, board)
+    line_of_sight(2, 3, n, board)
 
     print(board)
 
