073e845df93cc4502779b21c1a1e35efd473e721
Africa-t0t0 <geoorgex@gmail.com>
2022-04-08 03:58:24 -0400

    faltan muchas weas pero al menos funciona

--- main.py
+++ main.py
@@ -62,16 +62,33 @@ def n_queens():
     n=4
     queens = 0
     new_tree = Tree()
+    new_tree.create_node("root", "root")
     board = np.zeros((n, n), dtype=int)
     for x in range(0,n):
+        aux = 0
         for y in range(0,n):
             valid_position, board = line_of_sight(x, y, n, board)
             if valid_position == True:
                 print('posicion valida')
                 board[x,y] = 1
+                if queens == 0:
+                    new_tree.create_node((x,y), 'x'+str(queens), parent="root")
+                    queens += 1
+                    break
+                new_tree.create_node((x,y), 'x'+str(queens), parent='x'+str(queens-1))
                 queens += 1
+            # elif aux == n:        TODO: pensar en como hacer que la wea reemplace el ultimo que saco del arbol y le sume uno para tomar otro valor
+            #     aux2 = 0
+            #     for i in range(0,n):
+            #         if board[x, i] == 0:
+            #             aux2 += 1
+            #     if aux2 == n:
+            #         new_tree.delete_node('x'+str(queens-1))
             else:
                 print('posicion no valida')
+                aux+=1
     print(board)
+    new_tree.show()
+    print(board[2])
 
 n_queens()
